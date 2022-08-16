278d3bb825efd8e42e33626bf428ac06f402ee01
pamelacampbell <simocastudio@gmail.com>
2022-08-16 15:11:18 +0200

    evento tarot

--- js/code.js
+++ js/code.js
@@ -1,63 +1,47 @@
 "use strict";
 
 //codigo tarot
-
-// tomo el elemento
-let cartasTarot = document.getElementById("cartasTarot");
-
-//Defino la funcion del array de objetos de la baraja
-const obtenerCartas = () => [{
-        imagen: "../icons/card_1.png",
-        imagenDorso:"../icons/bk_card.png",
-        nombre: "El Proceso",
-        descripcion: "Estas en camino a encontrar tu planta ideal. Mira las señales, un potus esta por entrar a tu vida."
-    },
-    {
-        imagen: "../icons/card_2.png",
-        imagenDorso:"../icons/bk_card.png",
-        nombre: "El Suelo",
-        descripcion: "Estas list@ para dar un salto en tu vida. Atrévete a grandes planes y plantas, como el Ficus."
-    },
-    {
-        imagen: "../icons/card_3.png",
-        imagenDorso:"../icons/bk_card.png",
-        nombre: "El Crecimiento",
-        descripcion: "Estas en una etapa de contemplar los logros conseguidos. Agradecételo con una hermosa Calathea."
-    },
-];
-
-//evoco la funcion y la almaceno en cartas
-const cartas = obtenerCartas();
-
-//funcion para mezclar las cartas
-const mezclar = () => {
-    const barajaTarot = obtenerCartas();
-    barajaTarot.sort(() => Math.random() - 0.5);
-    return barajaTarot;
+//Defino el objeto carta
+class Carta {
+    constructor(numero, nombre, foto, descripcion) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.foto = foto;
+        this.descripcion = descripcion;
+    }
 }
 
-//creo las cartas en el HTML (DOM) usando la funcion de mezclar 
+//Array vacio para pushear los objetos de la funcion cargarCartas
+const barajas = [];
 
-const creadorCartas = () => {
-    const barajaTarot = mezclar();
-    for (const carta of barajaTarot) {
-        let cartaTarot = document.createElement("div");
-        let frente = document.createElement('img');
-        let dorso = document.createElement("img");
-        cartaTarot.className = "cartaTarot";
-        frente.className = "frente";
-        dorso.className = "dorso";
-        //le agrego la imagen al src
-        frente.src = carta.imagen;
-        dorso.src = carta.imagenDorso;
-        // le asigno un padre a los elemntos creados
-        cartasTarot.append(cartaTarot);
-        cartaTarot.append(frente);
-        cartaTarot.append(dorso);
-        //evento para dar vuelta las cartas
-        cartaTarot.addEventListener("click", (flipcard) => {
-            cartaTarot.classList.toggle("toogleCard");
-        });
+//Ejecuto la funcion para cargar la baraja
+cargarCartas();
+
+// Defino funcion que cargara las cartas al array
+function cargarCartas() {
+    barajas.push(new Carta(1, 'El Proceso', '../icons/card_1.png', 'Estas en camino a encontrar tu planta ideal. Mira las señales, un potus esta por entrar a tu vida.'));
+    barajas.push(new Carta(2, 'El Suelo', '../icons/card_2.png', 'Estas list@ para dar un salto en tu vida. Atrévete a grandes planes y plantas, como el Ficus.'));
+    barajas.push(new Carta(3, 'El Crecimiento', '../icons/card_3.png', 'Estas en una etapa de contemplar los logros conseguidos. Agradecételo con una hermosa Calathea.'));
+}
+//funcion para mezclar las cartas
+function mezclar(numero) {
+    let mezclarCartas = Math.floor(Math.random() * numero);
+    return mezclarCartas;
+}
+
+//tomo el boton para sacar una carta
+document.getElementById("sacarCarta").onclick = function () {
+    let posicionCarta = mezclar(3);
+    let cartaActual = barajas[posicionCarta];
+
+    //creo las cartas de la baraja con la funcion mezclar
+    document.getElementById("cartasTarot").innerHTML = `
+    <div class="card-body col-md-3">
+    <img class="card-img-top" src="${cartaActual.foto}" alt="Card image cap">
+        <h5 class="card-title">${cartaActual.nombre}</h5>
+        <p class="card-text"> ${cartaActual.descripcion}</p>
+        <button class="btn" id="miBoton"> link a cada planta </a>
+    </div>
+`;
     };
-};
-creadorCartas();
+
