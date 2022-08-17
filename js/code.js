1a07d908913cbd8f1dfa19497d442fd1ccd621bc
pamelacampbell <simocastudio@gmail.com>
2022-08-17 14:49:13 +0200

    css

--- js/code.js
+++ js/code.js
@@ -1,50 +1,4 @@
 
-//codigo tarot
-//Defino el objeto carta tarot
-class Carta {
-    constructor(numero, nombre, foto, descripcion) {
-        this.numero = numero;
-        this.nombre = nombre;
-        this.foto = foto;
-        this.descripcion = descripcion;
-    }
-}
-
-//Array vacio para pushear los objetos de la funcion cargarCartas
-const barajas = [];
-
-//Ejecuto la funcion para cargar la baraja
-cargarCartas();
-
-// Defino funcion que cargara las cartas al array
-function cargarCartas() {
-    barajas.push(new Carta(1, 'El Proceso', '../icons/card_1.png', 'Estas en camino a encontrar tu planta ideal. Mira las señales, un potus esta por entrar a tu vida.'));
-    barajas.push(new Carta(2, 'El Suelo', '../icons/card_2.png', 'Estas list@ para dar un salto en tu vida. Atrévete a grandes planes y plantas, como el Ficus.'));
-    barajas.push(new Carta(3, 'El Crecimiento', '../icons/card_3.png', 'Estas en una etapa de contemplar los logros conseguidos. Agradecételo con una hermosa Calathea.'));
-}
-//funcion para mezclar las cartas
-function mezclar(numero) {
-    let mezclarCartas = Math.floor(Math.random() * numero);
-    return mezclarCartas;
-}
-
-//tomo el boton para sacar una carta
-document.getElementById("sacarCarta").onclick = function () {
-    let posicionCarta = mezclar(3);
-    let cartaActual = barajas[posicionCarta];
-
-    //creo las cartas de la baraja con la funcion mezclar
-    document.getElementById("cartasTarot").innerHTML = `
-    <div class="card-body col-lg-3">
-    <img class="card-img-top" src="${cartaActual.foto}" alt="Card image cap">
-        <h5 class="card-title">${cartaActual.nombre}</h5>
-        <p class="card-text"> ${cartaActual.descripcion}</p>
-        <a href="../pages/tienda.html"><p class="text-center enlaceTexto">Encontrala acá </p></a>
-    </div>
-`;
-    };
-
-
    //codigo tienda
 
 //Defino el objeto producto
@@ -56,13 +10,39 @@ class Producto {
         this.foto = foto;
     }
 }
+
+//Defino el objeto Carrito
+class ElementoCarrito {
+    constructor(producto, cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+}
+
+
+//Definiciones de constante moneda
+ 
+ const estandarDolaresAmericanos = Intl.NumberFormat('en-US');
+
 //Array vacio para pushear los objetos de la funcion cargarProductos
 const productos = [];
 //Array vacio para pushear los objetos de la compra
-const itemsCarrito = [];
+const elementosCarrito = [];
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+
+const contenedorCarritoCompras = document.querySelector("#items")
+
+const contenedorFooterCarrito = document.querySelector("#footer");
 
 //Ejecuto la funcion para cargar los productos al array
 cargarProductos();
+
+//Ejecuto la funcion para el carrito
+//cargarCarrito();
+cargarCarrito();
+dibujarCarrito();
+dibujarCatalogoProductos();
 
 // Defino funcion que cargara los productos al array
 function cargarProductos() {
@@ -81,6 +61,82 @@ function cargarProductos() {
 }
 
 
+// funcion para crear el carrito
+
+f
+function cargarCarrito() {
+    /*let elementoCarrito = new ElementoCarrito(
+        new Producto(1, 'Muffin', 1.99, './img/muffin.jpg'),
+        1
+    );
+
+    elementosCarrito.push(elementoCarrito);*/
+}
+
+function dibujarCarrito() {
+
+    let sumaCarrito = 0;
+    contenedorCarritoCompras.innerHTML = "";
+
+    elementosCarrito.forEach(
+        (elemento) => {
+            let renglonesCarrito= document.createElement("tr");
+            
+            renglonesCarrito.innerHTML = `
+                <td>${elemento.producto.id}</td>
+                <td>${elemento.producto.nombre}</td>
+                <td><input id="cantidad-producto-${elemento.producto.id}" type="number" value="${elemento.cantidad}" min="1" max="1000" step="1" style="width: 50px;"/></td>
+                <td>$ ${elemento.producto.precio}</td>
+                <td>$ ${estandarDolaresAmericanos.format(elemento.producto.precio*elemento.cantidad)}</td>
+                <td><button id="eliminar-producto-${elemento.producto.id}" type="button" class="btn btn-danger"><i class="bi bi-trash-fill"></i></button></td>
+            `;
+
+            contenedorCarritoCompras.append(renglonesCarrito);
+
+            sumaCarrito+=elemento.cantidad*elemento.producto.precio;
+
+            //agregamos evento a carrito
+            let cantidadProductos = document.getElementById(`cantidad-producto-${elemento.producto.id}`);
+            
+            cantidadProductos.addEventListener("change", (e) => {
+                let nuevaCantidad = e.target.value;
+                elemento.cantidad = nuevaCantidad;
+                dibujarCarrito();
+            });
+
+            let borrarProducto = document.getElementById(`eliminar-producto-${elemento.producto.id}`);
+
+            borrarProducto.addEventListener("click", (e) => {
+                removerProductoCarrito(elemento);
+                dibujarCarrito();
+            });
+
+        }
+    );
+
+    //contenedorCarritoCompras.innerHTML = renglonesCarrito;
+    
+    if(elementosCarrito.length == 0) {
+        contenedorFooterCarrito.innerHTML = `
+            <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `;
+    } else {
+        contenedorFooterCarrito.innerHTML = `
+            <th scope="row" colspan="5">Total de la compra: $${estandarDolaresAmericanos.format(sumaCarrito)}</th>
+        `;
+    }
+
+}
+
+function removerProductoCarrito(elementoAEliminar) {
+    const elementosAMantener = elementosCarrito.filter((elemento) => elementoAEliminar.producto.id != elemento.producto.id);
+    elementosCarrito.length = 0;
+
+    elementosAMantener.forEach((elemento) => elementosCarrito.push(elemento));
+}
+
+
+
 //cards productos  de la tienda 
 let cards = document.getElementById("cards");
 for (const producto of productos) {
@@ -91,7 +147,7 @@ for (const producto of productos) {
         <img class="card-img-top" src="${producto.foto}" alt="Card image cap">
             <h5 class="card-title">${producto.nombre}</h5>
             <p class="card-text">$ ${producto.precio}</p>
-            <button class="btn" id="miBoton"> Lo quiero </a>
+            <button class="btn buttonStyles"> Lo quiero </a>
         </div>
     `;
     cards.append(card);
